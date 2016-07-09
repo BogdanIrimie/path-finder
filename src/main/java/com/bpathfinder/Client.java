95ff52beb1ac171360efd6c82837bab13bc11ab2
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-07-09 17:01:25 +0200

    Add JavaDoc.

--- src/main/java/com/bpathfinder/Client.java
+++ src/main/java/com/bpathfinder/Client.java
@@ -33,19 +33,23 @@ public class Client {
     }
 
 
+    /**
+     * Consider events happening in 1 second interval as happening at the same time.
+     *
+     * @param trackingRecords
+     */
     public void flattenTime(List<TrackingRecord> trackingRecords) {
         List<TrackingRecord> flatTime = new ArrayList<TrackingRecord>();
 
-        // the idea is to consider event that happen in 1 second interval as happening at the same time
+        // The idea is to consider event that happen in 1 second interval as happening at the same time
         trackingRecords.stream().forEach( trackingRecord -> {
             if (flatTime.size() >= 1) {
                 if (flatTime.get(0).getTime() + 1 > trackingRecord.getTime()) {
                     flatTime.add(trackingRecord);
                 }
                 else {
-                    //computeTrackingPointByAverage(flatTime);
-                    //computeTracePointByTrilateration(flatTime);
                     if (flatTime.stream().distinct().count() >= 3) {
+                        // TracePoint tp = computeTrackingPointByAverage(flatTime);
                         TracePoint tp = computeTracePointByTrilateration(flatTime);
                         pathTraces.add(tp);
                     }
@@ -64,33 +68,42 @@ public class Client {
         });
     }
 
-    private void computeTrackingPointByAverage(List<TrackingRecord> trackingRecords) {
+    /**
+     * Compute TracePoint by using average method.
+     *
+     * @param trackingRecordsAtSameTime is a list of tracking records that we can consider as happening at the same time.
+     * @return TracePoint obtain by averaging method.
+     */
+    private TracePoint computeTrackingPointByAverage(List<TrackingRecord> trackingRecordsAtSameTime) {
+        double x = 0, y = 0, time = 0;
+        int counter = 0;
 
-        if (trackingRecords.stream().distinct().count() >= 3) {
-            double x = 0, y = 0, time = 0;
-            int counter = 0;
+        // Only consider distinct tracking devices in order to not distort average results.
+        Iterator<TrackingRecord> distinctPoints = trackingRecordsAtSameTime.stream().distinct().collect(Collectors.toList()).iterator();
+        while (distinctPoints.hasNext()) {
+            TrackingRecord tr = distinctPoints.next();
+            x += tr.getxAxis();
+            y += tr.getyAxis();
+            time += tr.getTime();
+            counter++;
+        };
 
-            Iterator<TrackingRecord> distinctPoints = trackingRecords.stream().distinct().collect(Collectors.toList()).iterator();
-            while (distinctPoints.hasNext()) {
-                TrackingRecord tr = distinctPoints.next();
-                x += tr.getxAxis();
-                y += tr.getyAxis();
-                time += tr.getTime();
-                counter++;
-            };
-
-            pathTraces.add(new TracePoint(time/counter, x/counter, y/counter));
-
-        }
+        return new TracePoint(time/counter, x/counter, y/counter);
     }
 
-    private TracePoint computeTracePointByTrilateration(List<TrackingRecord> trackingRecords) {
-        double[][] positions = new double[trackingRecords.size()][2];
-        double[] distances = new double[trackingRecords.size()];
+    /**
+     * Compute TracePoint by using trilateration method.
+     *
+     * @param trackingRecordsAtSameTime is a list of tracking records that we can consider as happening at the same time.
+     * @return TracePoint obtain by trilateration.
+     */
+    private TracePoint computeTracePointByTrilateration(List<TrackingRecord> trackingRecordsAtSameTime) {
+        double[][] positions = new double[trackingRecordsAtSameTime.size()][2];
+        double[] distances = new double[trackingRecordsAtSameTime.size()];
         int recordCounter = 0;
         double time = 0;
 
-        Iterator<TrackingRecord> it = trackingRecords.iterator();
+        Iterator<TrackingRecord> it = trackingRecordsAtSameTime.iterator();
         while (it.hasNext()) {
             TrackingRecord tr = it.next();
             positions[recordCounter][0] = tr.getxAxis();
