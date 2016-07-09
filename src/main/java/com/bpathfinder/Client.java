e8559e3d4faf4a73051fa3041745fa1833b2e9dd
Irimie Bogdan <irimie.bogdan@outlook.com>
2016-07-09 16:50:42 +0200

    Use trilateration to compute more accurately the coordinates of trace points.

--- src/main/java/com/bpathfinder/Client.java
+++ src/main/java/com/bpathfinder/Client.java
@@ -4,7 +4,10 @@ import com.bpathfinder.dto.TracePoint;
 import com.bpathfinder.dto.TrackingRecord;
 import com.bpathfinder.parsers.CsvParser;
 import com.bpathfinder.parsers.ObjectsToCsv;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.lemmingapex.trilateration.NonLinearLeastSquaresSolver;
+import com.lemmingapex.trilateration.TrilaterationFunction;
+import org.apache.commons.math3.fitting.leastsquares.LeastSquaresOptimizer;
+import org.apache.commons.math3.fitting.leastsquares.LevenbergMarquardtOptimizer;
 
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -12,20 +15,42 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Client {
+    private static List<TracePoint> pathTraces = new ArrayList<TracePoint>();
+
     public static void main(String[] args) {
+        // parse CSV
         CsvParser csvParser = new CsvParser();
         List<TrackingRecord> trackingRecords = csvParser.parse();
 
+        // flatten time
+        Client client = new Client();
+        client.flattenTime(trackingRecords);
+
+        pathTraces.stream().forEach(System.out::println);
+
+        ObjectsToCsv objectsToCsv = new ObjectsToCsv();
+        objectsToCsv.writePojoToCsv(pathTraces);
+    }
+
+
+    public void flattenTime(List<TrackingRecord> trackingRecords) {
         List<TrackingRecord> flatTime = new ArrayList<TrackingRecord>();
 
-        // the idea is to consider event that happen in 1 second as happening at the same time
+        // the idea is to consider event that happen in 1 second interval as happening at the same time
         trackingRecords.stream().forEach( trackingRecord -> {
             if (flatTime.size() >= 1) {
                 if (flatTime.get(0).getTime() + 1 > trackingRecord.getTime()) {
                     flatTime.add(trackingRecord);
                 }
                 else {
-                    computeFlatTimeUnit(flatTime);
+                    //computeTrackingPointByAverage(flatTime);
+                    //computeTracePointByTrilateration(flatTime);
+                    if (flatTime.stream().distinct().count() >= 3) {
+                        TracePoint tp = computeTracePointByTrilateration(flatTime);
+                        pathTraces.add(tp);
+                    }
+
+
                     while (flatTime.size() > 0 && flatTime.get(0).getTime() + 1 < trackingRecord.getTime()) {
                         flatTime.remove(0);
                     }
@@ -35,22 +60,13 @@ public class Client {
             else {
                 flatTime.add(trackingRecord);
             }
-            System.out.println("Time flattened!");
+            // System.out.println("Time flattened!");
         });
-
-        pathTraces.stream().forEach(System.out::println);
-        System.out.println("Computer some triangulated points!");
-
-        ObjectsToCsv objectsToCsv = new ObjectsToCsv();
-        objectsToCsv.writePojoToCsv(pathTraces);
     }
 
-    private static List<TracePoint> pathTraces = new ArrayList<TracePoint>();
+    private void computeTrackingPointByAverage(List<TrackingRecord> trackingRecords) {
 
-    private static void computeFlatTimeUnit(List<TrackingRecord> trackingRecords) {
         if (trackingRecords.stream().distinct().count() >= 3) {
-            System.out.println("Can do triangulation!");
-
             double x = 0, y = 0, time = 0;
             int counter = 0;
 
@@ -66,5 +82,27 @@ public class Client {
             pathTraces.add(new TracePoint(time/counter, x/counter, y/counter));
 
         }
+    }
+
+    private TracePoint computeTracePointByTrilateration(List<TrackingRecord> trackingRecords) {
+        double[][] positions = new double[trackingRecords.size()][2];
+        double[] distances = new double[trackingRecords.size()];
+        int recordCounter = 0;
+        double time = 0;
+
+        Iterator<TrackingRecord> it = trackingRecords.iterator();
+        while (it.hasNext()) {
+            TrackingRecord tr = it.next();
+            positions[recordCounter][0] = tr.getxAxis();
+            positions[recordCounter][1] = tr.getyAxis();
+            distances[recordCounter++] = tr.getRssi();
+            time += tr.getTime();
+        }
+
+        NonLinearLeastSquaresSolver solver = new NonLinearLeastSquaresSolver(new TrilaterationFunction(positions, distances), new LevenbergMarquardtOptimizer());
+        LeastSquaresOptimizer.Optimum optimum = solver.solve();
+        double[] centroid = optimum.getPoint().toArray();
+
+        return new TracePoint(time/recordCounter, centroid[0], centroid[1]);
     }
 }
